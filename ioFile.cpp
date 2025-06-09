12854b0df001ea08f010aaa47e88c6da0296ba2f
AhmadNawfalRamadhan <Ahmadnawfalramadhan@gmail.com>
2025-06-09 10:08:32 +0700

    di int main membuat operasi file dalam mode menulis dan membaca

--- ioFile.cpp
+++ ioFile.cpp
@@ -4,5 +4,37 @@
 using namespace std;
 
 int main() {
-    
+    string baris;
+
+    //operasi file dalam mode menulis
+    ofstream outfile;
+    //menunjuk ke sebuah nama file untuk membuka 
+    outfile.open("contohfile.txt");
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+    //unlimited loop untuk menulis
+    while (true) {
+        cout << "- ";
+        //mendapatkan setiap karakter dalam satu baris
+        getline(cin, baris);
+        //loop akan berhenti jika anda memasukkan karakter q
+        if (baris == "q") break;
+        // menulis dan memasukan nilai dari 'baris' ke dalam file
+        outfile << baris << endl;
+    }
+    //selesai dalam menulis sekarang tutup filenya
+    outfile.close();
+
+    //operasi file dalam mode membaca
+    ifstream infile;
+    //menunjuk ke sebuah file untuk membuka
+    infile.open("contohfile.txt");
+
+    cout << endl << ">=Membuka dan membaca file " << endl;
+    //jika file ada maka
+    if (infile.is_open())
+    {
+        
+    }
 }
