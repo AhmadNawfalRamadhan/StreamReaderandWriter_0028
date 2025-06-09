8b02e008e887e36e999341e6d1f9c3c199cfe403
AhmadNawfalRamadhan <Ahmadnawfalramadhan@gmail.com>
2025-06-09 10:32:45 +0700

    manual exeption yang digunakan untuk mengalihkan

---
+++ manualExeption.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    try {
+        cout << "Selamat Belajar di Prodi TI UMY" << endl;
+        throw 0,5; //melemparkan sebuah integer maka
+        cout << "Pernyataa  tidak akan dieksekusi" << endl;
+    }
+    catch (int a) {
+        //blok ini akan dieksekusi
+        cout << "pengecualian akan dieksekusi" << endl;
+    }
+    catch
+}
