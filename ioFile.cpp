a570d3428ac56912c62ab0c944cf73cc8b8a6b60
AhmadNawfalRamadhan <Ahmadnawfalramadhan@gmail.com>
2025-06-09 10:11:32 +0700

    menutup file

--- ioFile.cpp
+++ ioFile.cpp
@@ -35,6 +35,15 @@ int main() {
     //jika file ada maka
     if (infile.is_open())
     {
+
+        //melakukan perulangan setiap baris
+        while (getline(infile, baris))
+        {
+            //dan tampilkan di sini
+            cout << baris << '\n'; 
+        }
+        //tutup file tersebut setelah selesai
+        infile.close();
         
     }
 }
